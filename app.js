cab30353bd433e5c9576d064309356f34c214b06
betaarqeng-dot <betaarqeng@gmail.com>
2025-09-19 12:21:20 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 (function() {
-   emailjs.init("0tXlVPYzdd05PES_1"); // Public Key
+  emailjs.init("0tXlVPYzdd05PES_1"); // Public Key
 })();
 
-document.getElementById("questionarioForm").addEventListener("submit", function(event) {
-    event.preventDefault();
+document.getElementById("questionario").addEventListener("submit", function(event) {
+  event.preventDefault();
 
-    emailjs.sendForm("service_vo4vkdf", "template_hpj29vj", this)
-        .then(function() {
-            alert("✅ Questionário enviado com sucesso!");
-            document.getElementById("questionarioForm").reset();
-        }, function(error) {
-            alert("❌ Erro ao enviar: " + JSON.stringify(error));
-        });
+  emailjs.sendForm("service_vo4vkdf", "template_hpj29vj", this)
+    .then(function() {
+      alert("✅ Questionário enviado com sucesso!");
+    }, function(error) {
+      alert("❌ Erro ao enviar. Verifique a configuração do EmailJS.");
+      console.error(error);
+    });
 });
